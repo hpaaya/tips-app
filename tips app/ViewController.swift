498a5162c6e1804e9d5c32f544c0802941473a5a
User14 <user14@user14de-imac.local>
2018-11-14 12:30:58 +0800

    Initial Commit

---
+++ tips app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tips app
+//
+//  Created by User14 on 2018/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
