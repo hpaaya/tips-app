29c2f84c777a743a306a0de9340fddc935b127bb
User09 <user09@user09de-imac.local>
2018-11-22 15:39:16 +0800

    Tips APP

--- tips app/ViewController.swift
+++ tips app/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NTDTextField: UITextField!
+    @IBOutlet weak var USDTextField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func USDcalculate(_ sender: Any) {
+        if let NTDText = NTDTextField.text, let NTD = Double(NTDText){
+            let USD = Double(NTD / 30.0)
+            USDTextField.text = "\(USD)"
+            if USD > 200{
+                label.text = "有錢人！！"
+            }
+            else{
+                label.text = "加油！多賺錢！"
+            }
+        }
+    }
+    @IBAction func NTDcalculate(_ sender: Any) {
+        if let USDText = USDTextField.text, let USD = Double(USDText){
+            let NTD = Double(USD * 30.0)
+            NTDTextField.text = "\(NTD)"
+            
+            if NTD > 5000{
+                label.text = "有錢人！！"
+            }
+            else {
+                label.text = "加油！多賺錢！"
+            }
+        }
+    }
+    
 }
-
